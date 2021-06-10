33f9e851e951735649b71e20dcf51958e28b2cb8
Loic Eickmayer <leickmay@z1r11p3.42lyon.fr>
2021-06-10 18:22:34 +0200

    ex00

---
+++ cpp00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leickmay <leickmay@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/10 15:38:42 by leickmay          #+#    #+#             */
+/*   Updated: 2021/06/10 17:47:59 by leickmay         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j(1);
+	char c;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (av[j])
+		{
+			while (av[j][i])
+			{
+				c = toupper(av[j][i]);
+				std::cout << c;
+				i++;
+			}
+			i = 0;
+			j++;
+		}
+		std::cout << std::endl;
+	}
+}
