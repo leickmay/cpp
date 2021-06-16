0a7960bf1edeb3973beef99a11f110f47cf8047a
Loic Eickmayer <leickmay@z1r11p3.42lyon.fr>
2021-06-16 11:33:19 +0200

    added includes

--- cpp01/ex01/ex01.cpp
+++ cpp01/ex01/ex01.cpp
@@ -6,9 +6,12 @@
 /*   By: leickmay <leickmay@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 11:25:38 by leickmay          #+#    #+#             */
-/*   Updated: 2021/06/16 11:25:40 by leickmay         ###   ########lyon.fr   */
+/*   Updated: 2021/06/16 11:32:31 by leickmay         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+#include <string>
 
 void	memoryLeak()
 {
